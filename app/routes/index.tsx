814eebd22a7ba285312c61979036e06ad052277c
Matthias Martin <github@stichwort-m.de>
2022-03-27 13:36:34 +0200

    denser ui

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -48,70 +48,57 @@ export default function Index() {
         </div>
         <div className="mt-8 xs:mt-12">
           <Form method="post" className="flex flex-col gap-y-6">
-            <div className="flex flex-col gap-y-4">
+            <div className="flex flex-col gap-y-2 xxs:gap-y-3 xs:gap-y-4">
               {Array(5)
                 .fill(0)
                 .map((_, i) => (
-                  <div key={i} className="flex flex-wrap items-end gap-2 xs:flex-nowrap">
-                    <div className="w-full shrink">
-                      <div className="mt-1">
-                        <input
-                          type="text"
-                          name={`${i}.char`}
-                          placeholder="Character"
-                          maxLength={1}
-                          autoCorrect="off"
-                          className="block w-full rounded-md border-gray-300 py-2 px-3 uppercase shadow-sm placeholder-shown:normal-case focus:border-indigo-500 focus:ring-indigo-500 xs:py-3 xs:px-4"
-                        />
-                      </div>
+                  <div key={i} className="flex items-end gap-1 xxs:gap-2">
+                    <div className="mt-1 w-full shrink">
+                      <input
+                        type="text"
+                        name={`${i}.char`}
+                        placeholder="A-Z"
+                        maxLength={1}
+                        autoCorrect="off"
+                        className="block w-full rounded-md border-gray-300 py-2 px-3 uppercase shadow-sm placeholder-shown:normal-case focus:border-indigo-500 focus:ring-indigo-500 xxs:py-3 xxs:px-4"
+                      />
                     </div>
-                    <div className='flex justify-between gap-2 grow'>
-                      {Array(5)
-                        .fill(0)
-                        .map((_, j) => (
-                          <label key={j}>
-                            <input type="checkbox" name={`${i}.${j}`} className="peer hidden" />
-                            <span className="box-content flex h-10 w-10 cursor-pointer items-center justify-center rounded-md border border-gray-300 text-gray-300 peer-checked:border-red-400 peer-checked:bg-red-400 peer-checked:text-white xs:h-12 xs:w-12">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="block h-8 w-8"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                strokeWidth={2}
-                              >
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                              </svg>
-                            </span>
-                          </label>
-                        ))}
-                    </div>
+                    {Array(5)
+                      .fill(0)
+                      .map((_, j) => (
+                        <label key={j}>
+                          <input type="checkbox" name={`${i}.${j}`} className="peer hidden" />
+                          <span className="box-content flex h-10 w-10 cursor-pointer items-center justify-center rounded-md border border-gray-300 text-gray-300 peer-checked:border-red-400 peer-checked:bg-red-400 peer-checked:text-white xxs:h-12 xxs:w-12">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="block h-8 w-8"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                              strokeWidth={2}
+                            >
+                              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                          </span>
+                        </label>
+                      ))}
                   </div>
                 ))}
             </div>
-            <div>
-              <label htmlFor="notAllowed" className="block text-sm font-medium text-gray-700">
-                Not allowed
-              </label>
-              <div className="mt-1">
-                <input
-                  type="text"
-                  name="notAllowed"
-                  id="notAllowed"
-                  autoCorrect="off"
-                  placeholder="Type in not allowed characters"
-                  className="block w-full rounded-md border-gray-300 py-3 px-4 uppercase shadow-sm placeholder-shown:normal-case focus:border-indigo-500 focus:ring-indigo-500"
-                />
-              </div>
-            </div>
-            <div>
-              <button
-                type="submit"
-                className="inline-flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                Get Words
-              </button>
-            </div>
+            <input
+              type="text"
+              name="notAllowed"
+              id="notAllowed"
+              autoCorrect="off"
+              placeholder="Type in not allowed characters"
+              className="block w-full rounded-md border-gray-300 py-3 px-4 uppercase shadow-sm placeholder-shown:normal-case focus:border-indigo-500 focus:ring-indigo-500"
+            />
+            <button
+              type="submit"
+              className="inline-flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              Get Words
+            </button>
           </Form>
         </div>
         {!!data?.words?.length && (
